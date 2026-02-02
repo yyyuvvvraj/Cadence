dd4ba338c9720adcea7e51516fa1c7045c8e01a5
SujalKishore <131854159+SujalKishore@users.noreply.github.com>
2026-02-02 23:13:35 +0530

    Update NotFound.jsx

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -20,7 +20,7 @@ const NotFound = () => {
         </div>
 
         <h2 className="text-2xl font-medium text-onBackground mb-2">
-          Page Not Found
+          404 Page Not Found
         </h2>
         <p className="text-onBackground/70 mb-8">
           The page you're looking for doesn't exist. Let's get you back!
